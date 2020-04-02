ae2fc44364bb9a5900a532a141bfcb8824a91009
unknown <btrn86@gmail.com>
2020-04-03 02:10:42 +0300

    add app structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './app.css'
-import ErrorBoundary from '../error-boundary'
-import  BooksoreService from '../../services/bookstore-service'
-import { BookstoreServiceProvider } from '../bookstore-service-context'
+import {withBookstoreService} from '../hoc'
 
-
-const App = () => {
+const App = ({bookstoreService}) => {
+    console.log(bookstoreService.getBooks())
     return (
-        <ErrorBoundary>
-            <BookstoreServiceProvider>
-                <div className="App">
-                    <h1>Hello World</h1>
-                </div>
-            </BookstoreServiceProvider>
-        </ErrorBoundary>
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
     );
 }
 
-export default App;
+export default withBookstoreService()(App);
